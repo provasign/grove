d1d3164b20165fda48c7dc33f063d317d9fac050
Tapabrata "Topo" Pal <topo.pal@gmail.com>
2026-06-07 09:35:24 -0400

    Fix Windows index test helper

--- internal/index/coverage_test.go
+++ internal/index/coverage_test.go
@@ -7,26 +7,7 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
-
-	"github.com/provasign/grove/internal/parser"
-	"github.com/provasign/grove/internal/store"
 )
-
-// helper to build an Indexer with isolated store
-func newIdx(t *testing.T) (*Indexer, func()) {
-	t.Helper()
-	dir := t.TempDir()
-	st, err := store.Open(filepath.Join(dir, "db.sqlite"))
-	if err != nil {
-		t.Fatal(err)
-	}
-	if err := st.Migrate(context.Background()); err != nil {
-		t.Fatal(err)
-	}
-	eng := parser.NewEngine()
-	idx := New(eng, st)
-	return idx, func() { _ = st.Close() }
-}
 
 func TestIndex_SkipsUnsupportedAndDirs(t *testing.T) {
 	idx, cleanup := newIdx(t)
